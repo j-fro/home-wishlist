60b7427494ef47325dc40c6c2b3954f804ee3ee7
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-30 16:12:30 -0600

    scaffolded out client

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,15 +5,33 @@ $(document).ready(function() {
 
 function enable() {
     // Set up event handlers
+    $('.to-admin').on('click', showAdmin);
+    $('.to-wishlist').on('click', showWishlist);
+}
+
+function showAdmin() {
+    console.log('showing admin');
+    getItems();
+    $('.view').hide();
+    $('.admin').show();
+}
+
+function showWishlist() {
+    console.log('showing wishlist');
+    getItems();
+    $('.admin').hide();
+    $('.view').show();
 }
 
 // GET from server
-function getData() {
+function getItems() {
     $.ajax({
-        url: '/',
+        url: '/getItems',
         type: 'GET',
         success: function(response) {
-            // Process results
+            console.log('wishlist:', response);
+            displayView(response);
+            displayAdmin(response);
         },
         error: function(error) {
             console.log("AJAX error:", error);
@@ -22,19 +40,53 @@ function getData() {
 }
 
 // POST to server
-function postData() {
+function addItem() {
     // Build new object
-    var objectToSend = {};
+    var objectToSend = {
+        name: $('#nameIn').val()
+    };
     // POST object
     $.ajax({
-        url: '/',
+        url: '/addItem',
         type: 'POST',
         data: objectToSend,
         success: function(response) {
-            // Process results
+            console.log('Response from server:', response);
+            getItems();
         },
         error: function(error) {
             console.log("AJAX error:", error);
         }
     });
+}
+
+function completeItem() {
+    console.log('completing an item', $(this));
+}
+
+function claimItem() {
+    console.log('claiming an item', $(this));
+}
+
+function updateItem(itemToSend) {
+    $.ajax({
+        url: '/updateItem',
+        type: 'PUT',
+        data: itemToSend,
+        success: function(response) {
+            console.log('Response from server:', response);
+            getItems();
+        },
+        error: function(error) {
+            console.log("AJAX error:", error);
+        }
+    });
+}
+
+function displayView(itemArray) {
+    console.log('displaying the view:', itemArray);
+}
+
+function displayAdmin(itemArray) {
+    console.log('displaying the admin:', itemArray);
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+Home Wishlist
+=============
+A todo-list-like web application to track items I need in my new place (or in general)
